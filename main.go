32fe0101232778a9a773aa8af7feae1fa82cf73f
4ce <@@@@@@@@@@>
2023-03-12 20:16:40 -0400

    verbose mode

--- main.go
+++ main.go
@@ -10,15 +10,17 @@ func main() {
 	http.HandleFunc("/play-video", func(w http.ResponseWriter, r *http.Request) {
 		videoUrl := r.URL.Query().Get("url")
 		if videoUrl != "" {
+			fmt.Printf("Playing video: %s\n", videoUrl)
 			cmd := exec.Command("mpv", videoUrl)
 			if err := cmd.Run(); err != nil {
 				fmt.Fprintf(w, "Error playing video: %s", err)
+			} else {
+				fmt.Fprintf(w, "Video played successfully")
 			}
-			fmt.Fprintf(w, "Video played successfully")
 		} else {
 			fmt.Fprintf(w, "Video URL not found in request")
 		}
 	})
-	fmt.Println("VidFix is running on localhost:9999/")
+	fmt.Println("Server listening on port 9999")
 	http.ListenAndServe(":9999", nil)
 }
